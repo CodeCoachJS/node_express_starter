10677eec65c7a7e0b0a7cf68c6b2fbbe2167b1ec
mangosing <seanmangosing@gmail.com>
2022-05-05 09:56:57 -0700

    clean up routes, tests and port

--- middlewares/validateUserBody.js
+++ middlewares/validateUserBody.js
@@ -1,13 +1,14 @@
 // we want to avoid requests that do not have the necessary information
 // middleware will intercept the request and fail early if the correct information is not available
 const validateUserBody = (req, res, next) => {
-    if (!req.body.id && !req.query.id) {
-        return res.status(400).send({ message: 'You must include an id' });
+    if (!req.body.id && !req.params.id) {
+      return res.status(400).send({ message: 'You must include an id' });
     }
     // next will continue the operation and pass the request to the next phase
     next();
-};
-
-module.exports = {
+  };
+  
+  module.exports = {
     validateUserBody,
-};
+  };
+  
