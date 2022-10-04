f909d171b6e62ea515d0a6fdac17787f48ec4b86
Brian Jenney <brianj@Brian-Jenney-MBP16-2019.local>
2022-10-04 16:27:08 -0700

    update middleware

--- middlewares/validateUserBody.js
+++ middlewares/validateUserBody.js
@@ -1,13 +1,13 @@
 // we want to avoid requests that do not have the necessary information
 // middleware will intercept the request and fail early if the correct information is not available
 const validateUserBody = (req, res, next) => {
-    if (!req.body.id && !req.params.id) {
-        return res.status(400).send({ message: 'You must include an id' });
-    }
-    // next will continue the operation and pass the request to the next phase
-    next();
+  if (![req.body.id, req.params.id].findLast((id) => id)) {
+    return res.status(400).send({ message: "You must include an id" });
+  }
+  // next will continue the operation and pass the request to the next phase
+  next();
 };
 
 module.exports = {
-    validateUserBody,
+  validateUserBody,
 };
